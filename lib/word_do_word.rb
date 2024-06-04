37b21af5edf421c851b63cbdbca38d041c62cadf
denigo360 <115095299+denigo360@users.noreply.github.com>
2024-06-04 15:54:07 +0300

    Add word_to_word gem

---
+++ lib/word_do_word.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "word_do_word/version"
+
+module WordDoWord
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/word_do_word/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WordDoWord
+  VERSION = "0.1.0"
+end
